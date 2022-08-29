87b59a15fd2b292b04a9626c7d6e8d06da651e0c
Setusudan <cryptiareta@gmail.com>
2022-08-29 17:48:32 +0200

    Add project files.

---
+++ EthLnyCli/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Irashaimaseeeeee");
+static string? GetInput()
+{
+    return Console.ReadLine();
+}
+
+string input = GetInput();
+
+if (input == null)
+{
+    Console.WriteLine("Invalid input");
+    return;
+}
+foreach (var word in input.Split(' '))
+{
+    switch (word)
+    {
+        case "react":
+            Process.Start("CMD.exe", "/K npx create-react-app");
+            break;
+        case "vue":
+            Process.Start("CMD.exe", "/K npm init vue@latest");
+            break;
+        case "vite":
+            Process.Start("CMD.exe", "/K npm create vite@latest");
+            break;
+        case "angular":
+            Process.Start("CMD.exe", "/K ng new");
+            break;
+        case "_":
+            Console.WriteLine("Invalid input");
+            return;
+    }
+
+
+
+}
+
